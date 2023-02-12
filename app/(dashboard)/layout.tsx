e56d8784811490bbdeee09f6a61f48ba1f299bd4
Marco Gerstmann <m.gerstmann@gmail.com>
2023-02-12 15:34:45 +0100

    Add layouts and style with tailwind

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import GlassPane from "@/components/GlassPane";
+import "@/styles/global.css";
+
+const DashboardRootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen candy-mesh p-6">
+        <GlassPane className="w-full h-full p-6 flex align-center container mx-auto">
+          <main className="w-full pl-6 h-full">{children}</main>
+        </GlassPane>
+      </body>
+    </html>
+  );
+};
